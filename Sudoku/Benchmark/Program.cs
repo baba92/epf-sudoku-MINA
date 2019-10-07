38ac5264191dd07ac607df96472f28d5c0a38f46
timnll <timothee.nouaille@epfedu.fr>
2019-10-07 15:22:41 +0200

    arborescence mais probleme de reference non reconnue

--- Sudoku/Benchmark/Program.cs
+++ Sudoku/Benchmark/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Benchmark
 {
@@ -7,6 +9,18 @@ namespace Benchmark
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string directory = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).ToString()).ToString()).ToString()).ToString()).ToString();
+            var sudokupath = Path.Combine(directory + "/data/Sudoku_Easy50.txt");
+            //string text = File.ReadAllText(sudokupath);
+            //Console.WriteLine(Sudoku.Parse(text)); //affiche un sudoku
+
+            List<Sudoku> allSudokus = Sudoku.ParseFile(sudokupath);
+            for (int i = 0; i < allSudokus.Count; i++)
+            {
+                Sudoku sudoku = (Sudoku)allSudokus[i];
+                Console.WriteLine(sudoku.ToString());
+            }
         }
     }
 }
